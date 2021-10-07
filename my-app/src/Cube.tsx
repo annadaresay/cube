58c5238523a001d6d614a012f79e9432182f39f6
Adrian Sergheev <sergheevdev@icloud.com>
2021-10-07 18:05:53 +0200

    best cube ever

--- my-app/src/Cube.tsx
+++ my-app/src/Cube.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { gql, useQuery, useSubscription } from "@apollo/client";
+import Cube3d from 'react-3d-cube';
 
 const get = gql`
   query Query {
@@ -37,7 +38,39 @@ const Cube = () => {
   console.log("getData: ", getData);
   console.log("subscribeData: ", subscribeData);
 
-  return <h1>Test</h1>;
+  return (
+    <div>
+          <h1>react-3d-cube</h1>
+          <h2>no children</h2>
+          <div
+          style={{
+              width: 300,
+              height: 300
+          }}
+          >
+          <Cube3d size={300} index="front" />
+          </div>
+          <h2>set children</h2>
+          <div
+          style={{
+              width: 300,
+              height: 300
+          }}
+          >
+          <Cube3d size={300} index="front">
+              <div>front</div>
+              <div>right</div>
+              <div>back</div>
+              <div>left</div>
+              <div>top</div>
+              <div>bottom</div>
+          </Cube3d>
+          </div>
+    </div>
+  );
 };
 
 export default Cube;
+
+
+
